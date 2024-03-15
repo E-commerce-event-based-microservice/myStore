12c1e6d10bd0517db7ff92ada6deda62f454ec24
Abdullah Khallouf <a.khallouf@outlook.com>
2024-03-15 22:46:51 +0100

    defined terraform API gateway and RDS instances resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+
+# API gateway
+resource "aws_instance" "APIG" {
+  ami           = local.EC2InstaceAMI
+  instance_type = local.EC2InstanceType
+   tags = {
+  #  Name = var.instance_name
+     Name = APIGateway
+   }
+}
+
+# user service RDS instance
+resource "aws_db_instance" "userService_RDS_instance" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t2.micro"
+
+  # TF_VAR_username
+  username            = var.userService_db_user
+  password            = var.userService_db_password
+  name                = var.userService_db_name
+  port                = 3306
+  skip_final_snapshot = true
+}
+
+# order service RDS instance
+resource "aws_db_instance" "orderService_RDS_instance" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t2.micro"
+
+  # TF_VAR_username
+  username            = var.orderService_db_user
+  password            = var.orderService_db_password
+  name                = var.orderService_db_name
+  port                = 3306
+  skip_final_snapshot = true
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,58 @@
+# input variables, can be passed during applying the apply command
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+
+locals{
+    APIGatewayPort = 8080
+    NotificationTopicName = "OrderCreated"
+    orderTopicName = "orderRecieved"
+    # Ubuntu 20.04 LTS // us-east-1
+    EC2InstaceAMI = "ami-011899242bb902164" 
+    EC2InstanceType = "t2.micro"
+}
+
+# like the return value
+
+output "instance_ip_addr" {
+  value = aws_instance.instance.private_ip
+}
+
+output "db_instance_addr" {
+  value = aws_db_instance.db_instance.address
+}
+
+#  user service database  related
+# at least password will be supplied as env variable
+variable "userService_db_user" {
+  description = "user service database user"
+  type        = string
+  default     = "root"
+}
+variable "userService_db_password" {
+  description = "user service database password"
+  type        = string
+}
+variable "userService_db_name" {
+  description = "name of the database"
+  type        = string
+  default     = "user"
+}
+
+#  user service database  related
+variable "orderService_db_user" {
+  description = "user service database user"
+  type        = string
+  default     = "root"
+}
+variable "orderService_db_password" {
+  description = "user service database password"
+  type        = string
+}
+variable "orderService_db_name" {
+  description = "name of the database"
+  type        = string
+  default     = "user"
+}
