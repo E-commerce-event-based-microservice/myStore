c56148c1174942982ea8efb9a5c8134cb34cce80
Abdullah Khallouf <a.khallouf@outlook.com>
2024-03-20 14:33:59 +0100

    created remoted s3 backend for terrafrom state and ignored local state files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,45 @@ terraform {
       version = "5.41.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "store-abood-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+# orphan - removed it with terraform state rm <resource>c
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "store-abood-terraform-state-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+name = "terraform-state-locking"
+billing_mode = "PAY_PER_REQUEST"
+hash_key = "lockID"
+
+attribute {
+  name = "lockID"
+  type = "S"
+}
+
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
 
 
 # API gateway
