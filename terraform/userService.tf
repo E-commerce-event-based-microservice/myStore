f86e415116aeebed5a856612afcce21a970a28d1
Abdullah Khallouf <a.khallouf@outlook.com>
2024-03-21 05:26:19 +0100

    test

--- terraform/userService.tf
+++ terraform/userService.tf
@@ -48,7 +48,7 @@ resource "aws_ecs_service" "userService" {
   task_definition      = aws_ecs_task_definition.userService.arn
   launch_type          = "FARGATE"
   scheduling_strategy  = "REPLICA"
-  desired_count        = 1
+  desired_count        = 2
   deployment_minimum_healthy_percent = 0
   #deployment_maximum_percent = 100     
   force_new_deployment = true
